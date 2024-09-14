b4732c9a8bcbe8a2a81fcc662355752b583d353f
Jason Moore <jason@rameyroad.com>
2024-09-14 14:00:41 -0400

    Sample pages

---
+++ jbtax-next/src/app/whowehelp/sample-one/page.tsx
@@ -0,0 +1,10 @@
+import { ComingSoon } from "@/views/shared/comingSoon";
+import { Fragment } from "react";
+
+export default function Page() {
+    return (
+        <Fragment>
+            <ComingSoon />
+        </Fragment>
+    );
+}
