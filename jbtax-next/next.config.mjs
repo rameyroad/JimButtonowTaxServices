3e59fb96aaecbdacfc5adc9652ca425fceb1cd33
Jason Moore <jason@rameyroad.com>
2024-09-15 14:29:53 -0400

    Working on build

--- jbtax-next/next.config.mjs
+++ jbtax-next/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
