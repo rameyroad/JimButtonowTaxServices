06637ca9938b35ec974b255279d9ce846b3ea50a
Sam <sjbutttonow@uncg.edu>
2024-09-18 21:09:02 -0400

    Checkpoint

--- jbtax-next/src/app/whowehelp/page.tsx
+++ jbtax-next/src/app/whowehelp/page.tsx
@@ -17,37 +17,64 @@ export default function Page() {
                     <div className="target-item">
                         <a href="whowehelp/target1">
                             <img src="/images/Home_-_father_son.webp" alt="Target 1" width={200} height={100} />
-                            <h2>Target 1</h2>
+                            <h2>IRS Audits</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                     <div className="target-item">
                         <a href="#">
                             <img src="/images/Home_-_backpacking_couple.webp" alt="Target 2" width={200} height={100} />
-                            <h2>Target 2</h2>
+                            <h2>Tax Debt Issues</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                     <div className="target-item">
                         <a href="#">
                             <img src="/images/Home_-_woman_eiffel-3.webp" alt="Target 3" width={200} height={100} />
-                            <h2>Target 3</h2>
+                            <h2>Unfiled Back Tax Returns</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                     <div className="target-item">
                         <a href="#">
                             <img src="https://placehold.co/300x200" alt="Target 4" width={300} height={200} />
-                            <h2>Target 4</h2>
+                            <h2>IRS Penalties</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                     <div className="target-item">
                         <a href="#">
                             <img src="https://placehold.co/300x200" alt="Target 5" width={300} height={200} />
-                            <h2>Target 5</h2>
+                            <h2>Payroll Tax Issues and</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                     <div className="target-item">
                         <a href="#">
                             <img src="https://placehold.co/300x200" alt="Target 6" width={300} height={200} />
-                            <h2>Target 6</h2>
+                            <h2>Business Tax Issues</h2>
+                            <p>Small text</p>
+                        </a>
+                    </div>
+                    <div className="target-item">
+                        <a href="#">
+                            <img src="https://placehold.co/300x200" alt="Target 7" width={300} height={200} />
+                            <h2>Dealing with the IRS</h2>
+                            <p>Small text</p>
+                        </a>
+                    </div>
+                    <div className="target-item">
+                        <a href="#">
+                            <img src="https://placehold.co/300x200" alt="Target 8" width={300} height={200} />
+                            <h2>IRS Account Issues</h2>
+                            <p>Small text</p>
+                        </a>
+                    </div>
+                    <div className="target-item">
+                        <a href="#">
+                            <img src="https://placehold.co/300x200" alt="Target 9" width={300} height={200} />
+                            <h2>Tax Firms</h2>
+                            <p>Small text</p>
                         </a>
                     </div>
                 </div>
