f942dc63f85d053b557d1329df1543f39b3b6899
Jason Moore <jason@rameyroad.com>
2024-10-21 17:46:00 -0400

    Who we help page

--- jbtax-next/src/app/whowehelp/page.tsx
+++ jbtax-next/src/app/whowehelp/page.tsx
@@ -1,84 +1,29 @@
+"use client";
+
+import Content from "@/views/cms/components/Content";
+
+import { DynamicPage } from "@/views/cms/types/dynamicPage";
 import { Fragment } from "react";
 
-export default function Page() {
-    return (
-        <Fragment>
-            {/* Who We Help Hero Section */}
-            <section className="wwh-hero">
-                <img src="/images/peru-scaled.jpeg" alt="Hero Background" className="wwh-hero-img" />
-                <div className="wwh-hero-content">
-                    <h1>Who We Help</h1>
-                </div>
-            </section>
+import "./who-we-help.css"; // Import the CSS file for testing purposes
 
-            {/* Target Images Section */}
-            <section className="target-images">
-                <div className="target-container">
-                    <div className="target-item">
-                        <a href="whowehelp/IRS_audits">
-                            <img src="/images/Home_-_father_son.webp" alt="Target 1" width={200} height={100} />
-                            <h2>IRS Audits</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="/images/Home_-_backpacking_couple.webp" alt="Target 2" width={200} height={100} />
-                            <h2>Tax Debt Issues</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="/images/Home_-_woman_eiffel-3.webp" alt="Target 3" width={200} height={100} />
-                            <h2>Unfiled Back Tax Returns</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 4" width={300} height={200} />
-                            <h2>IRS Penalties</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 5" width={300} height={200} />
-                            <h2>Payroll Tax Issues and</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 6" width={300} height={200} />
-                            <h2>Business Tax Issues</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 7" width={300} height={200} />
-                            <h2>Dealing with the IRS</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 8" width={300} height={200} />
-                            <h2>IRS Account Issues</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                    <div className="target-item">
-                        <a href="#">
-                            <img src="https://placehold.co/300x200" alt="Target 9" width={300} height={200} />
-                            <h2>Tax Firms</h2>
-                            <p>Small text</p>
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </Fragment>
+export default function Page({ params }: { params: { pageSlug: string } }) {
+    return (
+        <Content
+            pageSlug={"who-we-help"}
+            className="who-we-help-main"
+            renderHero={(activePage: DynamicPage) => (
+                <Fragment>
+                    {activePage?.primaryImage?.media?.publicUrl != null && (
+                        <Fragment>
+                            <div className="who-we-help-hero" style={{ paddingBottom: "200px;" }}>
+                                <img alt={activePage?.title} src={activePage?.primaryImage?.media?.publicUrl} />
+                            </div>
+                            <div className="who-we-help-title">{activePage.title}</div>
+                        </Fragment>
+                    )}
+                </Fragment>
+            )}
+        />
     );
 }
